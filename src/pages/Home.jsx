0db606d25aaf55dd71b73028c8da01c89be69dfd
Growthency <itsinjamul@gmail.com>
2025-12-11 21:59:54 +0600

    Homepage checked and done finally

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import Hero from "../components/Hero";
 import WhyLearn from "../components/WhyLearn";
 import Stats from "../components/Stats";
-import FeaturedLessons from "../components/FeaturedLessons"; // ইমপোর্ট করলাম
-import TopContributors from "../components/TopContributors"; // নতুন
-import MostPopular from "../components/MostPopular"; // নতুন
+import FeaturedLessons from "../components/FeaturedLessons"; 
+import TopContributors from "../components/TopContributors"; 
+import MostPopular from "../components/MostPopular"; 
 import usePageTitle from "../hooks/usePageTitle";
 
 const Home = () => {
@@ -12,19 +12,13 @@ const Home = () => {
     <div>
       <Hero></Hero>
 
-      {/* Featured Section */}
       <FeaturedLessons></FeaturedLessons>
 
-      {/* Extra Section 1: Dynamic Top Contributors */}
       <TopContributors></TopContributors>
 
-      {/* Static Section */}
       <WhyLearn></WhyLearn>
 
-      {/* Extra Section 2: Dynamic Most Popular */}
       <MostPopular></MostPopular>
-
-      {/* Static Stats */}
       <Stats></Stats>
     </div>
   );
